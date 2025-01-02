b5fd18ba9d737279be3b2c9ba6533da2f23e2180
Dario Guidi <dario.guidi954@gmail.com>
2025-01-02 12:23:12 +0100

    .

--- src/calculation/molecularMass.py
+++ src/calculation/molecularMass.py
@@ -2,25 +2,27 @@ from snippets.fileManagement import read_file_chemical_element
 from utils.generateChemicalFormula import generateFormula
 
 # Calcolo della massa molecolare dati i pesi atomici e i suoi pedici
-def calculate_molecular_mass(stoichiometric_coefficients,chemical_element):
+def calculate_molecular_mass(stoichiometric_coefficients, chemical_element):
     """
     I coefficienti stechiometrici e i pesi atomici devono coincidere 
     nelle varie liste come posizione ed ordine quindi
     """
 
-    Pa=0
-    Pm=0
-    FILENAME="data/chemical_element.txt"
+    Pa = 0
+    Pm = 0
+    FILENAME = "data/chemical_element.txt"
 
-    atomic_masses=[]
-    formula=""
+    atomic_masses = []
+    formula = ""  # Fixed typo here
 
+    # Loop to get atomic masses and generate formula
     for i in range(len(chemical_element)):
         atomic_masses.append(read_file_chemical_element(chemical_element[i]))
-        formuala+=generateFormula(chemical_element)
+        formula+=generateFormula(chemical_element[i])+str(int(stoichiometric_coefficients[i]))  # Include stoichiometric coefficients if needed
 
-
+    # Calculate molecular mass
     for i in range(len(atomic_masses)):
-        Pa=stoichiometric_coefficients[i]*atomic_masses[i]
-        Pm+=Pa
-    return f"Massa molecolare{Pm} e formula molecolare{formula}"
+        Pa = stoichiometric_coefficients[i] * atomic_masses[i]
+        Pm += Pa
+
+    return f"Massa molecolare: {Pm} e formula molecolare: {formula}"
